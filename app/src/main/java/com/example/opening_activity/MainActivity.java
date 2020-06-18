0b895944c505384d849ea7227d00d82f7fb4ecef
divyansh199 <dawaghmare@mitaoe.ac.in>
2020-06-19 02:02:46 +0530

    changing the activity by click

---
+++ app/src/main/java/com/example/opening_activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.opening_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button) findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    private void openActivity2() {
+
+            Intent intent = new  Intent(this,Activity2.class);
+            startActivity(intent); //here this method is used to add the activity
+    }
+
+}
